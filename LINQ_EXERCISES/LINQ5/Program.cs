f7efdb6e8afb80a5402155a60c27eda6742d1fde
maciejdarlak <maciej.darlak@gmail.com>
2020-02-11 14:14:44 +0100

    Added LINQ6

---
+++ LINQ_EXERCISES/LINQ5/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program in C# Sharp to display numbers, multiplication of number with frequency and frequency of a number of giving array.
+
+using System;
+using System.Linq;
+
+namespace LINQ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 1, 9, 2, 3, 7, 4, 5, 6, 8, 7, 6, 3, 4, 5, 2 };
+
+            var repeatNumber = array.GroupBy(a => a);
+
+
+            foreach (var item in repeatNumber)
+            {
+                Console.WriteLine($"{ item.Key } { item.Sum() } { item.Count() }");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LINQ_EXERCISES/LINQ6/Program.cs
@@ -0,0 +1,33 @@
+﻿//Write a program in C# Sharp to find the string which starts and ends with a specific character. 
+
+using System;
+using System.Linq;
+
+
+// StartsWith () and EndsWith (endLetter) catch start and end characters even though it's a string, not a char
+
+namespace LINQ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string[] array = new string[] { "ROME","LONDON","NAIROBI","CALIFORNIA","ZURICH","NEW DELHI","AMSTERDAM","ABU DHABI", "PARIS"
+
+
+                string startLetter = "A";
+                string endLetter = "M";
+
+                var word = array.Select(a => a).Where(a => a.StartsWith(startLetter) && a.EndsWith(endLetter));
+
+                foreach (var item in word)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
