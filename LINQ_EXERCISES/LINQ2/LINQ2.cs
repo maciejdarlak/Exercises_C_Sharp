944814ed9b2ed1a8370982e576a3d2030e5d1ae0
maciejdarlak <maciej.darlak@gmail.com>
2020-02-11 11:57:56 +0100

    Added empty LINQ exercise 2.

---
+++ LINQ_EXERCISES/LINQ2/LINQ2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ2
+{
+    class LINQ2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
