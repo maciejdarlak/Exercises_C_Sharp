93b79c6209d9d614870b7ebe60528c6b0475033b
maciejdarlak <maciej.darlak@gmail.com>
2020-02-11 14:26:53 +0100

    Added LINQ7

---
+++ LINQ_EXERCISES/LINQ7/LINQ7.cs
@@ -0,0 +1,32 @@
+﻿//Write a program in C# Sharp to Accept the members of a list through the keyboard and display the members more than a specific value.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// Use Parse() to replace string to int
+// Use LINQ to select data
+
+namespace LINQ7
+{
+    class LINQ7
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Write a number");
+            int y = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int> { 0, 1, 2, 56, 32, 23 };
+
+            var newList = list.Select(a => a).OrderBy(a => a).Where(a => a > y).ToList();
+
+            foreach (var item in newList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
