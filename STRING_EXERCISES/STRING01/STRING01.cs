151128351526d714b2aa01f2b6ea039cc7d65035
maciejdarlak <maciej.darlak@gmail.com>
2020-02-12 15:52:45 +0100

    Added file

---
+++ STRING_EXERCISES/STRING01/STRING01.cs
@@ -0,0 +1,24 @@
+﻿//Write a C# Sharp program to compare two strings in following three different ways produce three different results
+
+//Break the sentence into words (including spaces) and arrange from the end
+
+using System;
+
+namespace STRING01
+{
+    class STRING01
+    {
+        static void Main(string[] args)
+        {
+            string[] str = "Ala ma kota".Split(new[] { "" }, StringSplitOptions.None);
+            string reversStr = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversStr += str[i].ToString();
+            }
+            Console.WriteLine(reversStr.ToString());
+            Console.ReadLine();  
+        }
+    }
+}
