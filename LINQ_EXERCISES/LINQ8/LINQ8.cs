edbb3480a15ada41403b89f7d27732b981d53225
maciejdarlak <maciej.darlak@gmail.com>
2020-02-12 08:56:21 +0100

    Added LINQ8

--- LINQ_EXERCISES/LINQ8/LINQ8.cs
+++ LINQ_EXERCISES/LINQ8/LINQ8.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿//Write a program in C# Sharp to display the top n-th records
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQ8
 {
-    class Program
+    class LINQ8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> list = new List<int>() { 5, 7, 13, 24, 6, 9, 8, 50 };
+
+            Console.WriteLine("Write a number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            var newList = list.Select(a => a).Where(a => a > x).ToList();
+
+            foreach (var item in newList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
