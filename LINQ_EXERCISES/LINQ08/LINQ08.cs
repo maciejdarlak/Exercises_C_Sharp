20a79710a0234ea457332a0babd13ae0875bc9a0
maciejdarlak <maciej.darlak@gmail.com>
2020-02-14 15:46:20 +0100

    Added LINQ8 file

--- LINQ_EXERCISES/LINQ08/LINQ08.cs
+++ LINQ_EXERCISES/LINQ08/LINQ08.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Write a program in C# Sharp to find the uppercase words in a string
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
 
 namespace LINQ08
 {
@@ -6,7 +11,13 @@ namespace LINQ08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string a = "ALICE has a nice CAT";
+            var b = a.Split(' ').Where(a => a == a.ToUpper());
+            foreach (var item in b)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
         }
     }
 }
