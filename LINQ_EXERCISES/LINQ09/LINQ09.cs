59785c49ac9a95aeb43eba4f5995b7fbce97d0fd
maciejdarlak <maciej.darlak@gmail.com>
2020-02-15 08:46:33 +0100

    Added LINQ9 file

--- LINQ_EXERCISES/LINQ09/LINQ09.cs
+++ LINQ_EXERCISES/LINQ09/LINQ09.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿//Write a program in C# Sharp to convert a string array to a string
 
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+/* The String.join() method connects elements of a specified array or collection members 
+   using a specific separator between each element or member (e.g. ", ").
+   The Select () method replaces array elements with the String type just in case.*/
 namespace LINQ09
 {
     class LINQ09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] array = new string[] { "cat", "hamster", "dog", "lion" };
+            var x = String.Join("' ", array.Select(a => a.ToString()).ToArray());
+            Console.WriteLine(x);
+            Console.Read();
         }
     }
 }
