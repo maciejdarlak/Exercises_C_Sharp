f040eb7b5d8ba7669fb96a10c271b6cbaeef8374
maciejdarlak <maciej.darlak@gmail.com>
2020-02-15 12:21:12 +0100

    Added LINQ11 file

---
+++ LINQ_EXERCISES/LINQ11/LINQ11.cs
@@ -0,0 +1,24 @@
+﻿//Write a program in C# Program to Count File Extensions and Group it using LINQ.
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+//Grouped array elements, then (Count ()) element of each group (Key).
+namespace LINQ11
+{
+    class LINQ11
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { ".xml", ".pdf", ".epub", ".mp3", ".flac", ".mp3", ".xml" };
+            var array2 = array.GroupBy(a => a).Select(a => new { Name = a.Key, MainCount = a.Count() }).ToArray();
+
+            foreach (var item in array2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.Read();
+        }
+    }
+}
