ac9ece8d7fd0829fa1be6a618ff3b17cc3c69e39
maciejdarlak <maciej.darlak@gmail.com>
2020-02-16 10:59:37 +0100

    Added STRING04 file

--- STRING_EXERCISES/STRING04/STRING04.cs
+++ STRING_EXERCISES/STRING04/STRING04.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Write a C# Sharp program to compare the current string instance with another string. 
+
+using System;
+
+//You only need to check if this is true using the logical operator "||" (answer "true" or "false").
 
 namespace STRING04
 {
@@ -6,7 +10,12 @@ namespace STRING04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Insert a number: ");
+            int input = Convert.ToInt32(Console.Read());
+
+            if (input > 0)
+                Console.WriteLine(input % 3 == 0 || input % 7 == 0);
+            Console.ReadKey();
         }
     }
 }
