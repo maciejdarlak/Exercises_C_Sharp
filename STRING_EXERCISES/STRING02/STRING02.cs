4a88bbd63695946c8a4920e7bef4c874a0d74461
maciejdarlak <maciej.darlak@gmail.com>
2020-02-16 10:50:19 +0100

    Added STRIN02 file

--- STRING_EXERCISES/STRING02/STRING02.cs
+++ STRING_EXERCISES/STRING02/STRING02.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//Write a C# Sharp program to perform and ordinal comparison of two strings that only differ in case.
+
+using System;
 
 namespace STRING02
 {
@@ -6,7 +8,15 @@ namespace STRING02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] firstArray = { 1, 3, -5, 4 };
+            int[] secondArray = { 1, 4, -5, -2 };
+
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.Write(firstArray[i] * secondArray[i] + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
