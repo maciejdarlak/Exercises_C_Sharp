5671f894e8fa8d22f421ecbf3aca5338996e7843
maciejdarlak <maciej.darlak@gmail.com>
2020-06-15 17:53:21 +0200

    Added Routing Exercices file (Model, Controller)

---
+++ ROUTING_EXERCISES/ROUTING_EXERCISES_01/Controllers/CountryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ROUTING_EXERCISES_01.Models;
+
+namespace ROUTING_EXERCISES_01.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Product overview");
+        }
+
+        public IActionResult List()
+        {
+            List<Country> countries = new List<Country>()
+            {
+                new Country() { Id=1, Name="Spain", City="Barcelona" },
+                new Country() { Id=2, Name="italy", City="Rome" }
+            };
+            return View(countries);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return Content("Product details for #" + id);
+        }
+
+        public IActionResult Word()
+        {
+            return View();
+        }
+    }
+}
---
+++ ROUTING_EXERCISES/ROUTING_EXERCISES_01/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ROUTING_EXERCISES_01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
