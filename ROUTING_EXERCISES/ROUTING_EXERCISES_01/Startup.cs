c05bda27520d855b2ab7d3d841b970c2662dddb0
maciejdarlak <maciej.darlak@gmail.com>
2020-06-16 15:01:53 +0200

    no message

--- ROUTING_EXERCISES/ROUTING_EXERCISES_01/Startup.cs
+++ ROUTING_EXERCISES/ROUTING_EXERCISES_01/Startup.cs
@@ -50,7 +50,7 @@ namespace ROUTING_EXERCISES_01
             {
                 endpoints.MapControllerRoute(  // This is No.1 because is upper.
                     name: "Country",
-                    pattern: "{controller=Country}/{action=List}/{id?}");
+                    pattern: "{controller=Home}/{action=Privacy}/{id?}");
 
                 endpoints.MapControllerRoute( // This is the next one because is lower.
                     name: "default",
