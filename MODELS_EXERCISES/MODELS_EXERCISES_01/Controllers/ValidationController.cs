9c7f18bd4ea6b3f3c25f9470adbaebfc0a9d4931
maciejdarlak <maciej.darlak@gmail.com>
2020-06-18 15:06:21 +0200

    Added Model Validation files.

---
+++ MODELS_EXERCISES/MODELS_EXERCISES_01/Controllers/ValidationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MODELS_EXERCISES_01.Models;
+
+
+namespace MODELS_EXERCISES_01.Controllers
+{
+    public class ValidationController : Controller
+    {
+        [HttpGet]
+        public IActionResult SimpleValidation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SimpleValidation(WebUser webUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Thank you.");
+            }
+            else
+                return View(webUser);
+        }
+    }
+}
---
+++ MODELS_EXERCISES/MODELS_EXERCISES_01/Models/WebUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MODELS_EXERCISES_01.Models
+{
+    public class WebUser
+    { 
+        [Required(ErrorMessage = "You must enter a value for the First Name field!")]
+        [StringLength(25, ErrorMessage = "The First Name must be no longer than 25 characters!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "You must enter a value for the Last Name field!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "The Last Name must be between 3 and 50 characters long!")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "You must enter a value for the Mail Address field!")]
+        [EmailAddress(ErrorMessage = "Please enter a valid e-mail address!")]
+        public string MailAddress { get; set; }
+    }
+}
