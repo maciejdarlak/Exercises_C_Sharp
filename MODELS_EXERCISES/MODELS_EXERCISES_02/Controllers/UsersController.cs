ab70534df4d713f189ba95445c0943e1cd7970e2
maciejdarlak <maciej.darlak@gmail.com>
2020-06-19 15:57:52 +0200

    Added Client-side validation

---
+++ MODELS_EXERCISES/MODELS_EXERCISES_02/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MODELS_EXERCISES_02.Models;
+
+
+namespace MODELS_EXERCISES_02.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(WebUser webUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Thank you.");
+            }
+            else
+            {
+                return View(webUser);
+            }
+        }
+    }
+}
---
+++ MODELS_EXERCISES/MODELS_EXERCISES_02/Models/WebUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MODELS_EXERCISES_02.Models
+{
+    public class WebUser
+    {
+        [Required]
+        [StringLength(25)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string MailAddress { get; set; }
+    }
+}
