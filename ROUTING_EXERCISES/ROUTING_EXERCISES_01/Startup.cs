f5810d1784ef9eb02359b713d3cc21eaacd27651
maciejdarlak <maciej.darlak@gmail.com>
2020-06-23 18:39:17 +0200

    no message

--- ROUTING_EXERCISES/ROUTING_EXERCISES_01/Startup.cs
+++ ROUTING_EXERCISES/ROUTING_EXERCISES_01/Startup.cs
@@ -20,13 +20,11 @@ namespace ROUTING_EXERCISES_01
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +34,7 @@ namespace ROUTING_EXERCISES_01
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
