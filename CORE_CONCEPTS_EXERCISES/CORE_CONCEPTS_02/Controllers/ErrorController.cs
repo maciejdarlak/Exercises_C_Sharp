37b64adf632db23ea6e7491068083d5ac2f20370
maciejdarlak <maciej.darlak@gmail.com>
2020-06-23 15:18:55 +0200

    no message

--- CORE_CONCEPTS_EXERCISES/CORE_CONCEPTS_02/Controllers/ErrorController.cs
+++ CORE_CONCEPTS_EXERCISES/CORE_CONCEPTS_02/Controllers/ErrorController.cs
@@ -15,16 +15,20 @@ namespace CORE_CONCEPTS_02.Controllers
         public IActionResult Index()
         {
             {
-                var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>(); // IExceptionHandlerPathFeature - to access information about the exception.
-                if (exceptionHandlerPathFeature?.Error is FileNotFoundException) // Info for webmaster
+                // IExceptionHandlerPathFeature - to access information about the exception.
+                var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+                // Info for webmaster
+                if (exceptionHandlerPathFeature?.Error is FileNotFoundException) 
                 {
                     return Content("File error thrown");
                 }
-                if (exceptionHandlerPathFeature?.Path == "/index") // Info for webmaster
+                // Info for webmaster
+                if (exceptionHandlerPathFeature?.Path == "/index") 
                 {
                     return Content("File error thrown from home page");
                 }
-                return Content("We're so sorry, but an error just occurred! It has been logged and we'll try to get it fixed ASAP!"); // Info for User
+                // Info for User
+                return Content("We're so sorry, but an error just occurred! It has been logged and we'll try to get it fixed ASAP!"); 
             }
         }
     }
