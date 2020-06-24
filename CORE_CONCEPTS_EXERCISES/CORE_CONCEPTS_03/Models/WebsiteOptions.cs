8f800012443f2c417e2d2da973a4b1f5ccbfdb24
maciejdarlak <maciej.darlak@gmail.com>
2020-06-24 14:32:30 +0200

    Added a class (object in appsettings.json file).

---
+++ CORE_CONCEPTS_EXERCISES/CORE_CONCEPTS_03/Models/WebsiteOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CORE_CONCEPTS_03.Models
+{
+    public class WebsiteOptions // This is an object addded to appsettings.json file.
+    {
+        public WebsiteOptions() { }
+        public string Title { get; set; }
+        public double Version { get; set; }
+    }
+}
