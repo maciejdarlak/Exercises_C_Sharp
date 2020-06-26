da73df4cef820b0f30e1d922d33d2992d5d7ccbc
maciejdarlak <maciej.darlak@gmail.com>
2020-06-26 16:42:26 +0200

    Added CookiesController.

---
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_03/Controllers/CookiesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HTTPCONTEXT_03.Controllers
+{
+    public class CookiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            {
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
+                return Content("Welcome, new visitor!");
+            }
+            else
+            {
+                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
+                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+            }
+
+        }
+    }
+}
