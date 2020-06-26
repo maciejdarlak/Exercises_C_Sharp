7f4dfa964cfdef2b10e4a5d2c8a28f9352602e16
maciejdarlak <maciej.darlak@gmail.com>
2020-06-26 14:02:29 +0200

    no message

---
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_02/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HTTPCONTEXT_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
