7b6f0ee5ac904d731ece502dc2c30b202ac78c5a
maciejdarlak <maciej.darlak@gmail.com>
2020-06-29 15:48:45 +0200

    no message

--- HTTPCONTEXT_EXERCISES/HTTPCONTEXT_03/Controllers/CookiesController.cs
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_03/Controllers/CookiesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 
 namespace HTTPCONTEXT_03.Controllers
@@ -10,10 +11,12 @@ namespace HTTPCONTEXT_03.Controllers
     public class CookiesController : Controller
     {
         public IActionResult Index()
-        {
+        {          
             if (!HttpContext.Request.Cookies.ContainsKey("first_request")) // The first user visit
             {
-                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString()); // Making cookies + present date
+                CookieOptions cookieOptions = new CookieOptions(); // It allows you to adjust several important aspects of your cookie, e.g. how long it should stay alive and stuff like domain(s) and path.
+                cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddDays(7)); // Adding expire time
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString(), cookieOptions); // Making cookies + present date
                 return Content("Welcome, new visitor!");
             }
             else // The next user visit
