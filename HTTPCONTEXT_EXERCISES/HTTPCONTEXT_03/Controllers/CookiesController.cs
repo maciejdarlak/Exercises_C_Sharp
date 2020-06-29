60af9807095c470e53d19869b9dca7606758e0f6
maciejdarlak <maciej.darlak@gmail.com>
2020-06-29 15:37:49 +0200

    no message

--- HTTPCONTEXT_EXERCISES/HTTPCONTEXT_03/Controllers/CookiesController.cs
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_03/Controllers/CookiesController.cs
@@ -11,17 +11,17 @@ namespace HTTPCONTEXT_03.Controllers
     {
         public IActionResult Index()
         {
-            if (!HttpContext.Request.Cookies.ContainsKey("first_request"))
+            if (!HttpContext.Request.Cookies.ContainsKey("first_request")) // The first user visit
             {
-                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString());
+                HttpContext.Response.Cookies.Append("first_request", DateTime.Now.ToString()); // Making cookies + present date
                 return Content("Welcome, new visitor!");
             }
-            else
+            else // The next user visit
             {
-                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]);
-                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString());
+                DateTime firstRequest = DateTime.Parse(HttpContext.Request.Cookies["first_request"]); // Change the format to DateTime (file_request is a key, datetime is a value).
+                return Content("Welcome back, user! You first visited us on: " + firstRequest.ToString()); // Change the format to String
             }
-
         }
     }
 }
+
