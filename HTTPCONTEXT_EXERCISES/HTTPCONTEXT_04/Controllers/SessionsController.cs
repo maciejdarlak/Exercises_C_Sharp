dfdeb44b3f32849a19a243a936b9bd9f7888128d
maciejdarlak <maciej.darlak@gmail.com>
2020-06-30 15:41:16 +0200

    Added session to file.

---
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_04/Controllers/SessionsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HTTPCONTEXT_04.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index() // This method tries to get the session data with the GetString() method, using "Name" as the key. 
+        {
+            string name = HttpContext.Session.GetString("Name");
+            return View(model: name);
+        }
+
+        public IActionResult SaveName(string name)
+        {
+            HttpContext.Session.SetString("Name", name); // This method passing in "Name" as the key and the submitted "name" as the value. 
+            return RedirectToAction("Index"); // Then redirects to Index method where the inscription is visible.
+        }
+    }
+}
--- HTTPCONTEXT_EXERCISES/HTTPCONTEXT_04/Startup.cs
+++ HTTPCONTEXT_EXERCISES/HTTPCONTEXT_04/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace HTTPCONTEXT_04
 {
     public class Startup
@@ -20,13 +21,16 @@ namespace HTTPCONTEXT_04
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession(options => // 1. This method should be added.
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+            }); 
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,9 +40,12 @@ namespace HTTPCONTEXT_04
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
                 app.UseHsts();
             }
+
+            app.UseSession(); // 2. This method should be added.
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -50,7 +57,7 @@ namespace HTTPCONTEXT_04
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Sessions}/{action=Index}/{id?}");
             });
         }
     }
