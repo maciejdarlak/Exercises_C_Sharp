9c63cdf4d1d4f529ad7db5c12e77f4db7b6ac02d
maciejdarlak <maciej.darlak@gmail.com>
2020-07-02 15:48:19 +0200

    no message

--- VIEW_COMPONENTS_EXERCISES/VIEW_COMPONENTS_01/Components/Cart.cs
+++ VIEW_COMPONENTS_EXERCISES/VIEW_COMPONENTS_01/Components/Cart.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; // It has to be added.
 
 
 namespace VIEW_COMPONENTS_01.Components
---
+++ VIEW_COMPONENTS_EXERCISES/VIEW_COMPONENTS_01/Components/Cart2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace VIEW_COMPONENTS_01.Components
+{
+    public class Cart2 : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return Content("This is from <h2>View Component</h2>");
+        }
+    }
+}
