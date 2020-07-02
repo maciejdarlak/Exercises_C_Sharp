339fc1e841e6f5568659045a60164a91510a64c7
maciejdarlak <maciej.darlak@gmail.com>
2020-07-02 15:15:38 +0200

    Added components folder and "Cart" file.

---
+++ VIEW_COMPONENTS_EXERCISES/VIEW_COMPONENTS_01/Components/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace VIEW_COMPONENTS_01.Components
+{
+    public class Cart : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "This is from View Component";
+        }
+    }
+}
