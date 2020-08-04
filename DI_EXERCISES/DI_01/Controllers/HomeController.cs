8427522d422715005f11ec966e82437823c33986
maciejdarlak <maciej.darlak@gmail.com>
2020-08-04 19:06:05 +0200

    Added a new controller.

---
+++ DI_EXERCISES/DI_01/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+using DI_01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DI_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Repository().Products); //References directly to the Products property when an object is created.
+        }
+    }
+}
