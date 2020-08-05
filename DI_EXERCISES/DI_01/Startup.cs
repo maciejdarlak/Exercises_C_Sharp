6dcd772e580d029936997eebd9a447fc30a1dd3f
maciejdarlak <maciej.darlak@gmail.com>
2020-08-05 20:50:59 +0200

    Added DI

--- DI_EXERCISES/DI_01/Startup.cs
+++ DI_EXERCISES/DI_01/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DI_01.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,7 @@ namespace DI_01
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IRepository, Repository>();
             services.AddControllersWithViews();
         }
 
