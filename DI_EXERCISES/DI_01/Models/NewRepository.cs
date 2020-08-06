640c32a303b4681f424d8288e76678e159738a7e
maciejdarlak <maciej.darlak@gmail.com>
2020-08-06 14:56:19 +0200

    A replacement model has been added for connection to IRepository.

---
+++ DI_EXERCISES/DI_01/Models/NewRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI_01.Models
+{
+    public class NewRepository : IRepository //Adding this interface is essential - it is linked to any model in the Startup file.
+    {
+        private Dictionary<string, Product> products;
+        public NewRepository()
+        {
+            products = new Dictionary<string, Product>();
+            new List<Product>
+            {
+                new Product { Name = "Dog Shoes", Price = 1M },
+            }.ForEach(p => AddProduct(p)); //Method AddProduct creates keys for valuaes in dictionary products.
+        }
+        public IEnumerable<Product> Products => products.Values;
+        public Product this[string name] => products[name];
+        public void AddProduct(Product product) => products[product.Name] = product;
+        public void DeleteProduct(Product product) => products.Remove(product.Name);
+    }
+}
