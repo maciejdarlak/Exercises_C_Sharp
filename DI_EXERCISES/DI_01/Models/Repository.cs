007e1758cf1595f20324317926af12010829d549
maciejdarlak <maciej.darlak@gmail.com>
2020-08-11 14:00:43 +0200

    The class has changed its dependency on another interface in the constructor

--- DI_EXERCISES/DI_01/Models/Repository.cs
+++ DI_EXERCISES/DI_01/Models/Repository.cs
@@ -8,21 +8,23 @@ namespace DI_01.Models
 {
     public class Repository : IRepository //Adding this interface is essential - it is linked to any model in the Startup file.
     {
-        private Dictionary<string, Product> products;
-        public Repository()
+        private IStorage storage;
+        public Repository(IStorage repo) //IRepository object in turn depends upon IStorage object.
         {
-            products = new Dictionary<string, Product>();
-            new List<Product>
-            {
+            storage = repo;
+            new List<Product> {
                 new Product { Name = "Women Shoes", Price = 99M },
                 new Product { Name = "Skirts", Price = 29.99M },
                 new Product { Name = "Pants", Price = 40.5M }
             }.ForEach(p => AddProduct(p)); //Method AddProduct creates keys for valuaes in dictionary products.
         }
-            public IEnumerable<Product> Products => products.Values;
-            public Product this[string name] => products[name];
-            public void AddProduct(Product product) => products[product.Name] = product;
-            public void DeleteProduct(Product product) => products.Remove(product.Name);
 
+        public IEnumerable<Product> Products => storage.Items;
+
+        public Product this[string name] => storage[name];
+
+        public void AddProduct(Product product) => storage[product.Name] = product;
+
+        public void DeleteProduct(Product product) => storage.RemoveItem(product.Name);
     }
 }
