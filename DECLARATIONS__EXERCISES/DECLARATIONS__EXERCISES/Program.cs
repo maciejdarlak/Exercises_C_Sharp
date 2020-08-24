59a6f10646db485cfc05ed9bdd3c79029bf9f530
maciejdarlak <maciej.darlak@gmail.com>
2020-08-24 21:15:35 +0200

    Added a new project.

---
+++ DECLARATIONS__EXERCISES/DECLARATIONS__EXERCISES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DECLARATIONS__EXERCISES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DECLARATIONS__EXERCISES/DECLARATIONS__EXERCISES_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DECLARATIONS__EXERCISES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
