8277802d2e279ecc7f5e469ea43ad40d3e81c5a4
maciejdarlak <maciej.darlak@gmail.com>
2020-08-26 13:35:23 +0200

    Add a new project.

---
+++ _CSHARP_CLASSES__EXERCISES/_CSHARP_CLASSES__EXERCISES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _CSHARP_CLASSES__EXERCISES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ _CSHARP_CLASSES__EXERCISES/_CSHARP_CLASSES__EXERCISES_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _CSHARP_CLASSES__EXERCISES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
