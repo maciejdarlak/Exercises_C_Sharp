605c4f4d2fbeebfcc23baf1725d0b06dfa8754f0
maciejdarlak <maciej.darlak@gmail.com>
2020-08-26 14:57:09 +0200

    Added this, base and contructor codes.

--- _CSHARP_CLASSES__EXERCISES/_CSHARP_CLASSES__EXERCISES_01/Program.cs
+++ _CSHARP_CLASSES__EXERCISES/_CSHARP_CLASSES__EXERCISES_01/Program.cs
@@ -8,5 +8,68 @@ namespace _CSHARP_CLASSES__EXERCISES_01
         {
             Console.WriteLine("Hello World!");
         }
+
+
+
+
+        class Bar
+        {
+            public Bar(Foo obj)
+            {
+            }
+        }
+
+        class Foo
+        {
+            private Bar bar;
+
+            public Foo()
+            {
+                bar = new Bar(this);
+            }
+        }
+
+
+
+
+
+        class Baza
+        {
+            int pole;
+        }
+
+        class Pochodna : Baza
+        {
+            int pole;
+            public Pochodna()
+            {
+                base.pole = 25; 
+                pole = 100; 
+            }
+        }
+
+
+
+
+        class Class1
+        {
+            protected int value;
+
+            public Class1(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        class Class2 : Class1
+        {
+            int value2;
+
+            public Class2(int value, int value2)
+                : base(value)
+            {
+                this.value2 = value2;
+            }
+        }
     }
 }
