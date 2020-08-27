52a4b9de744f1f31a4e986b81bf63afa9b26dd49
maciejdarlak <maciej.darlak@gmail.com>
2020-08-27 11:13:19 +0200

    Added a new project.

---
+++ _CSHARP_STRUCTURES__EXERCISES/_CSHARP_STRUCTURES__EXERCISES_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _CSHARP_STRUCTURES__EXERCISES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
