75a31ef35953b9d7dde897e73c902fb54b463f70
maciejdarlak <maciej.darlak@gmail.com>
2020-08-28 15:36:02 +0200

    no message

--- _CSHARP_INHERITANCE__EXERCISES/_CSHARP_CLASSES__EXERCISES_01/Program.cs
+++ _CSHARP_INHERITANCE__EXERCISES/_CSHARP_CLASSES__EXERCISES_01/Program.cs
@@ -6,7 +6,29 @@ namespace _CSHARP_CLASSES__EXERCISES_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rectangle = new Square();
+            if (rectangle is Square)
+            {
+                Console.WriteLine("Success");
+            }            
+        }
+
+        public class Rectangle
+        {
+            public int x = 10, y = 10;
+            public int GetArea()
+            {
+                return x * y;
+            }
+        }
+
+        public class Square : Rectangle
+        {
+            public int x = 5, y = 5;
+            public int GetArea()
+            {
+                return x * y;
+            }
         }
     }
 }
