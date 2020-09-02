c8651bf1fb4d8b16d2d1a9bc36ad6c4e52a06028
maciejdarlak <maciej.darlak@gmail.com>
2020-09-02 18:32:06 +0200

    Added a downcast example.

---
+++ _CSHARP_INHERITANCE__EXERCISES/_CSHARP_CLASSES__EXERCISES_03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _CSHARP_CLASSES__EXERCISES_03
+{
+    class Program //Downcast example
+    {
+        static void Main(string[] args)
+        {         
+            Employee employee = new Employee(); 
+            Employee employeeManager = new Manager(); 
+
+            if (employee is Manager) Console.WriteLine("Employee is a manager."); //Not OK
+            if (employeeManager is Manager) Console.WriteLine("EmployeeManager is a manager.");   //OK
+        }
+
+        public class Employee { }
+
+        public class Manager : Employee { }
+    }
+}
