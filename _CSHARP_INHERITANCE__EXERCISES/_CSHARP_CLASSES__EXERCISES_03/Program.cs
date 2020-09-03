7b2e18ff31182b25723e69f0bee62ff475c3e3e1
maciejdarlak <maciej.darlak@gmail.com>
2020-09-03 19:29:05 +0200

    no message

--- _CSHARP_INHERITANCE__EXERCISES/_CSHARP_CLASSES__EXERCISES_03/Program.cs
+++ _CSHARP_INHERITANCE__EXERCISES/_CSHARP_CLASSES__EXERCISES_03/Program.cs
@@ -10,20 +10,19 @@ namespace _CSHARP_CLASSES__EXERCISES_03
             Employee employee = new Employee(); 
             Employee employee2 = new Manager(); 
 
+            //Way No.1
             if (employee is Manager) Console.WriteLine("Employee is a manager."); //Not OK
             if (employee2 is Manager) Console.WriteLine("Employee2 is a manager.");   //OK
 
-            //Or
-
+            //Way No.2
             Employee someEmployee = employee as Manager;
             if (someEmployee != null)   Console.WriteLine("Employee is a manager.");
-
             Employee someEmployee2 = employee2 as Manager;
             if (someEmployee2 != null)   Console.WriteLine("Employee2 is a manager.");
         }
 
-        public class Employee { }
 
+        public class Employee { }
         public class Manager : Employee { }
     }
 }
