8ab9c4bf955056fdef46cf839a073723ba4a74f4
maciejdarlak <maciej.darlak@gmail.com>
2020-09-08 14:44:55 +0200

    no message

--- _CSHARP_INHERITANCE__EXERCISES/_CSHARP_INHERITANCE__EXERCISES_05/Program.cs
+++ _CSHARP_INHERITANCE__EXERCISES/_CSHARP_INHERITANCE__EXERCISES_05/Program.cs
@@ -3,40 +3,31 @@ using System.ComponentModel;
 
 namespace _CSHARP_INHERITANCE_EXERCISES_05
 {
-    class Program
+    class Program  //Virtual methods
     {
         static void Main(string[] args)
         {
             A a = new A();
             B b = new B();
-            C c = new C();
-            Console.WriteLine($"{c.Name}");
+
+            Console.WriteLine($"{ b.Work(b.Name) }");
         }
 
         public class A
         {
             public string Name = "A";
-            virtual public void Work(string Name)  //virtual
+            virtual public string Work(string Name)  //Virtual
             {
-                Console.WriteLine($"{ Name } works.");
+                return ($"{ Name }");
             }
         }
 
         public class B : A
         {
             public string Name = "B";
-            override public void Work(string Name)  //override
+            override public string Work(string Name)  //Override
             {
-                Console.WriteLine($"{ Name } works.");
-            }
-        }
-
-        public class C : A
-        {
-            public string Name = "C";
-            public void Work(string Name)  //override
-            {
-                Console.WriteLine($"{ Name } works.");
+                return ($"{ Name }");
             }
         }
     }
