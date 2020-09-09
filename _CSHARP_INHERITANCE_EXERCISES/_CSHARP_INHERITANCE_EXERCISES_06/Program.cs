7b70e455425a97c7e491e07a8bd2688926b121cb
maciejdarlak <maciej.darlak@gmail.com>
2020-09-09 14:53:32 +0200

    no message

---
+++ _CSHARP_INHERITANCE_EXERCISES/_CSHARP_INHERITANCE_EXERCISES_06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _CSHARP_INHERITANCE_EXERCISES_06
+{
+   
+    class Program //Abstract class
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.DoSth();
+        }
+
+
+        abstract public class A
+        {
+            abstract public void DoSth();
+        }
+
+        public class B : A
+        {
+            override public void DoSth()
+            {
+                Console.WriteLine("B");
+            }
+        }
+    }
+}
