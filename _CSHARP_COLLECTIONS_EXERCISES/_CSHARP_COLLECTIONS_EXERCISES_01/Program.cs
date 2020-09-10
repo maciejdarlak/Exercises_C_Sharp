a24526b8c46427c23ff3f8843866c739375c94cf
maciejdarlak <maciej.darlak@gmail.com>
2020-09-10 16:23:08 +0200

    no message

--- _CSHARP_COLLECTIONS_EXERCISES/_CSHARP_COLLECTIONS_EXERCISES_01/Program.cs
+++ _CSHARP_COLLECTIONS_EXERCISES/_CSHARP_COLLECTIONS_EXERCISES_01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _CSHARP_COLLECTIONS_EXERCISES_01
 {
@@ -6,12 +7,62 @@ namespace _CSHARP_COLLECTIONS_EXERCISES_01
     {
         static void Main(string[] args)
         {
-            int[] a = { 1, 2, 3, 4, 5 };
+            List<Person> people = new List<Person>();
+            people.Add(new Person(1, "Jan", "Kowalski", 24));
+            people.Add(new Person(2, "Jan", "Nowak", 26));
+            people.Add(new Person(3, "Anna", "Gruszka", 28));
+            //People count
+            Console.WriteLine(people.Count);
 
+            //Break line
+            Console.WriteLine("\n");
 
-            foreach (var item in a)
+            //People's names
+            foreach (var person in people)
             {
-                Console.WriteLine(a.Length);
+                Console.WriteLine(person.FirstName + " " + person.LastName);
+            }
+
+            //Break line
+            Console.WriteLine("\n");
+
+            //Finding a specific person
+            Person personAge26 = people.Find(a => a.Age == 26);
+            Console.WriteLine(personAge26.ToString());
+
+            //Break line
+            Console.WriteLine("\n");
+
+            //Finding specyfic Names
+            List<Person> janList = people.FindAll(a => a.FirstName.Equals("Jan"));
+            Console.WriteLine(janList.ToString());                                    
+
+            //As above but used a loop
+            foreach (var item in people)
+            {
+                if (item.FirstName == "Jan")
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }
+            }
+        }
+
+
+
+
+        public class Person
+        {
+            public int PersonId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+
+            public Person(int nPersonId, string sFirstName, string sLastName, int nAge)
+            {
+                PersonId = nPersonId;
+                FirstName = sFirstName;
+                LastName = sLastName;
+                Age = nAge;
             }
         }
     }
