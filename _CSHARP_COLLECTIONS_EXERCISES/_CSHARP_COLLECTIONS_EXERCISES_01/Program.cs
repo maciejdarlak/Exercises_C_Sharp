e1504365e534b4863a8b6705c16e767fedbd4600
maciejdarlak <maciej.darlak@gmail.com>
2020-09-10 11:52:09 +0200

    no message

---
+++ _CSHARP_COLLECTIONS_EXERCISES/_CSHARP_COLLECTIONS_EXERCISES_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _CSHARP_COLLECTIONS_EXERCISES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine(a[3]);
+
+            foreach (var item in a)
+            {
+                Console.WriteLine(a.GetLength(item));
+            }
+        }
+    }
+}
---
+++ _CSHARP_STRUCTURAL_INSTRUCTIONS_EXERCISES/_CSHARP_STRUCTURAL_INSTRUCTIONS_EXERCISES_01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace _CSHARP_STRUCTURAL_INSTRUCTIONS_EXERCISES_01
+{
+    class Program //Switch
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Possibilities();
+        }
+
+
+        public class A
+        {
+            public void Possibilities()
+            {
+                string mark = "Porsche";
+                switch (mark)
+                {
+                    case "BMW":
+                        Console.WriteLine("You have chosen BMW.");
+                        break;
+                    case "Porsche":
+                        Console.WriteLine("You have chosen Porsche.");
+                        break;
+                    case "Audi":
+                        Console.WriteLine("You have chosen Audi.");
+                        break;
+                    default:
+                        Console.WriteLine("You have chosen another car.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
