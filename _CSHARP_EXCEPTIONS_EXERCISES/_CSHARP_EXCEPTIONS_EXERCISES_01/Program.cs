a72ac08fdafb0ec14e0a664614a2eec1ba9bd06d
maciejdarlak <maciej.darlak@gmail.com>
2020-09-18 12:02:56 +0200

    no message

---
+++ _CSHARP_EXCEPTIONS_EXERCISES/_CSHARP_EXCEPTIONS_EXERCISES_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _CSHARP_EXCEPTIONS_EXERCISES_01  //Divide by zero exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new A(1, 0);  //Result: 1 / 0
+            Console.WriteLine(a.result);
+        }
+
+        public class A
+        {
+            public int result = 0;
+            public A (int x, int y)
+            {
+                try
+                {
+                    result = x / y;
+                }
+                catch (DivideByZeroException Ex)
+                {
+                    Console.WriteLine("Exception: {0}" , Ex);
+                }
+                
+            }
+        }
+    }
+}
---
+++ _CSHARP_EXCEPTIONS_EXERCISES/_CSHARP_EXCEPTIONS_EXERCISES_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _CSHARP_EXCEPTIONS_EXERCISES_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
