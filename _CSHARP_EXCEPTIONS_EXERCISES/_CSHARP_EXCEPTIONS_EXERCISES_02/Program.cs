2c21715f2ab85a5bde2082d0b52b9c785a059bea
maciejdarlak <maciej.darlak@gmail.com>
2020-09-18 14:34:46 +0200

    no message

--- _CSHARP_EXCEPTIONS_EXERCISES/_CSHARP_EXCEPTIONS_EXERCISES_02/Program.cs
+++ _CSHARP_EXCEPTIONS_EXERCISES/_CSHARP_EXCEPTIONS_EXERCISES_02/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace _CSHARP_EXCEPTIONS_EXERCISES_02
+namespace _CSHARP_EXCEPTIONS_EXERCISES_02  //My own exception
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Age userAge = new Age();
+
+            try  //Here it is looking for an exception.
+            {
+                userAge.checkAge(10);
+            }
+            catch(MyOwnException ex)  //if it faund it...
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public class Age  
+        {
+            int age = 0;
+
+            public void checkAge(int a)
+            {
+                age = a;
+
+                if (age < 0)
+                {
+                    throw (new MyOwnException("Age cannot be less than 0."));  //"Throw" requires object which includes some exception class.
+                }
+                else
+                    Console.WriteLine("Correct age");
+            }
+        }
+
+        public class MyOwnException : ApplicationException  //My own exception heriting from ApplicationException class.
+        {
+            public MyOwnException(string text) : base(text)  //So the parameter requires text.
+            {
+
+            }
         }
     }
 }
