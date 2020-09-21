396ff20d327f2254d3d42f1a5a7f8e0f9336ada0
maciejdarlak <maciej.darlak@gmail.com>
2020-09-21 13:16:59 +0200

    no message

---
+++ _CSHARP_DELEGATES_EXERCISES/_CSHARP_DELEGATES_EXERCISES_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _CSHARP_DELEGATES_EXERCISES_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            static int Method1(int X)
+            {
+                return X * X;
+            }
+
+            static int Method2(int X)
+            {
+                return X / X;
+            }          
+
+
+            A a = new A(Method1);
+
+            Console.WriteLine(a(10));
+
+
+            a = Method2;
+
+            Console.WriteLine(a(10));
+
+        }
+
+        public delegate int A(int X);
+    }
+}
+    
