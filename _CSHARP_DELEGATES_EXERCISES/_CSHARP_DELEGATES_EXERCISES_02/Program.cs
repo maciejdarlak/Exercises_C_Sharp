759736acd718b36396581fb99306d4d658d41144
maciejdarlak <maciej.darlak@gmail.com>
2020-09-21 15:46:40 +0200

    no message

---
+++ _CSHARP_DELEGATES_EXERCISES/_CSHARP_DELEGATES_EXERCISES_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _CSHARP_DELEGATES_EXERCISES_02  //Delegate as a method parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = Transform(10, Square);
+            int B = Transform(2, Volume);
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+        }
+        public static int Square(int a) => a * a;  //1
+        public static int Volume(int a) => a * a * a;  //1
+
+        public delegate int Operation(int x);  //2
+
+        public static int Transform(int value, Operation operation)  //3
+        {
+            int result = operation(value);
+            return result;
+        }     
+    }
+}
