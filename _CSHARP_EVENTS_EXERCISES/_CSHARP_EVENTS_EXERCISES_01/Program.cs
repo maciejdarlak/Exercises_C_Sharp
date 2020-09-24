40732409fa600beeeee83381731be139e46f0d33
maciejdarlak <maciej.darlak@gmail.com>
2020-09-24 15:28:03 +0200

    no message

---
+++ _CSHARP_EVENTS_EXERCISES/_CSHARP_EVENTS_EXERCISES_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _CSHARP_EVENTS_EXERCISES_01
+{
+    class Program
+    {
+        public class Inscription
+        {
+            //Declare delegate
+            public delegate void ChangeDelegate(int x);
+            //Declare event
+            public event ChangeDelegate ChangeEvent;
+
+            public void ChangeMethod(int y)
+            {
+                if (ChangeEvent != null)
+                {
+                    Console.WriteLine("1");
+                }
+                else
+                {
+                    Console.WriteLine("2");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Inscription inscription = new Inscription();
+            inscription.ChangeEvent += new Inscription.ChangeDelegate(inscription.ChangeMethod); //Adding to event list the delegate with chosen method as a parameter.
+            inscription.ChangeMethod(10);
+            Console.WriteLine(); // Default value for the "Inscription" class is "null" so the answer is "2".
+        }
+    }
+}
