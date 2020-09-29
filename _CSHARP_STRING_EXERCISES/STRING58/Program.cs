f41ff089c7dd3061f66098b29122aa161e686fef
maciejdarlak <maciej.darlak@gmail.com>
2020-09-29 15:14:09 +0200

    no message

---
+++ _CSHARP_STRING_EXERCISES/STRING58/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace STRING58 //How to use the Reverse() method of array class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 2, 3, 4, 5 };
+            int[] B = A;
+
+            foreach (var i in A)
+            {
+                Console.Write(i);
+            }
+
+            Console.WriteLine(); //A gap line
+
+            Array.Reverse(B);
+
+            foreach (var j in B)
+            {
+                Console.Write(j);
+            }
+        }
+    }
+}
