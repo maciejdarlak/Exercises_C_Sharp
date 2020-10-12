c705ab58454a563600c41fc84a4cfc45ca1b14e4
maciejdarlak <maciej.darlak@gmail.com>
2020-10-12 18:15:17 +0200

    no message

---
+++ _CSHARP_STRING_EXERCISES/STRING62/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace STRING62
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
