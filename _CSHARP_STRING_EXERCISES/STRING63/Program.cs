894f9c6f66c4144a609bc947a2d87e904bf43e3b
maciejdarlak <maciej.darlak@gmail.com>
2020-10-14 11:54:08 +0200

    Replace result types - list with string.

--- _CSHARP_STRING_EXERCISES/STRING63/Program.cs
+++ _CSHARP_STRING_EXERCISES/STRING63/Program.cs
@@ -19,24 +19,22 @@ namespace STRING63 //How to remove duplicate characters from a string?
         public class A
         {
             public string str;
-            public List<char> singleCharactersList = new List<char>();
+            public string result = string.Empty; //A new one
 
             public void RemovedDuplicate()
             {
-                char[] charactersArray = str.ToCharArray();
-
-                foreach (var character in charactersArray)
+                foreach (var character in str) //Character is automatically char type
                 {
-                    if (character != ' ') 
+                    if (character != ' ') //Gap elemination
                     {
-                        if (!singleCharactersList.Contains(character))
+                        if (!result.Contains(character))
                         {
-                            singleCharactersList.Add(character);
+                            result += character;
                         }
                     }
                 }
 
-                foreach (var character in singleCharactersList)
+                foreach (var character in result)
                 {
                     Console.Write("{0}", character) ;
                 }
