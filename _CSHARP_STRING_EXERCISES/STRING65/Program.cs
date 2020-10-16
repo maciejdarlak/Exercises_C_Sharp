240d12e83e6057105069027107417ee63bee2c56
maciejdarlak <maciej.darlak@gmail.com>
2020-10-16 15:28:00 +0200

    no message

---
+++ _CSHARP_STRING_EXERCISES/STRING65/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace STRING65
+{
+    class Program //How to perform Left circular rotation of an array?
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] {  1, 2, 3 };
+            A a = new A();
+            a.RotateLeft(array);
+            Console.WriteLine();
+        }
+
+        public class A
+        {
+            public void RotateLeft(int[] array)
+            {
+                int size = array.Length;
+                int temp;
+
+                for (int j = size - 1; j > 0; j--)
+                {
+                    temp = array[size - 1];
+                    array[array.Length - 1] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+
+                foreach (int num in array)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+        }
+    }
+}
