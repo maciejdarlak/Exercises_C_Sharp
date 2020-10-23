5e2292151c4bed11a5a8069a4ab06bf75f54690d
maciejdarlak <maciej.darlak@gmail.com>
2020-10-23 14:27:11 +0200

    no message

---
+++ _CSHARP_DECLARATIONS_EXERCISES/_CSHARP_DECLARATIONS_EXERCISES_04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace _CSHARP_DECLARATIONS_EXERCISES_04 //How to find third largest integer in an array using only one loop?
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] NumberArray = new int[] { 8, 10, 3, 12, 7, 5 };
+            A a = new A();
+            var result = a.ThirdBiggestNumber(NumberArray);
+            Console.WriteLine(result);
+        }
+
+        public class A
+        {
+            public int ThirdBiggestNumber(int[] numberArray)
+            {
+                foreach (var num in numberArray)
+                {
+                    if (num > 0 && numberArray.Length > 2)
+                    {
+                        Array.Sort(numberArray);
+                    }                
+                }
+                return numberArray[numberArray.Length - 3];
+            }
+        }
+    }
+}
