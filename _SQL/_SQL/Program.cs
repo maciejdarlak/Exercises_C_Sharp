796cd9318fc11d654d15d8fbec4dfd004fea0c1a
maciejdarlak <maciej.darlak@gmail.com>
2020-11-04 17:32:21 +0100

    no message

---
+++ _SQL/_SQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
