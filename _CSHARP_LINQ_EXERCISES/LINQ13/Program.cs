d8e034b0cbab165e6679de916fbdcaef1b6108c4
maciejdarlak <maciej.darlak@gmail.com>
2020-11-10 19:03:46 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ13 //Write a query that returns top 5 numbers from the list of integers in descending order.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> A = new List<int> { 78, -9, 0, 23, 54, 21, 7, 86 };
+            var fiveNumbers = A.OrderByDescending(x => x).Take(5);
+
+            foreach (var item in fiveNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
