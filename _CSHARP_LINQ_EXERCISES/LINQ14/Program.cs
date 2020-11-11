3854211809cfa63a2ef6c5ddc0a6db347ae305f8
maciejdarlak <maciej.darlak@gmail.com>
2020-11-11 19:56:15 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ14 //Write a query that returns letters and their frequencies in the string.
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string word = "inseption";
+            var letters = word.GroupBy(x => x);
+
+            foreach (var x in letters)
+            {
+                Console.Write($"Letter {x.Key} occurs {x.Count()} time(s), ");
+            }
+        }
+    }
+}
+
+
