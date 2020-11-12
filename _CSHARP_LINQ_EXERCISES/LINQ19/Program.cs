01c7948c1d864a0e23c662b32bb4ca6ed0537dd0
maciejdarlak <maciej.darlak@gmail.com>
2020-11-12 15:55:56 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ19/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> A = new List<string>() { "learn", "current", "deal" };
+            var B = A.Select(x => x.Contains("ea") ? x.Replace("ea", "XXX") : x);
+            foreach (var item in B)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
