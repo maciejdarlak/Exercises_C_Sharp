0f582155ecb518bee848256ecbff63b8b1eef092
maciejdarlak <maciej.darlak@gmail.com>
2020-11-12 15:05:04 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LINQ16
+{
+    class Program //Write a query that returns words at least 5 characters long and make them uppercase.
+    {
+        static void Main(string[] args)
+        {
+            List<string> A = new List<string>() { "cat", "tiger" };
+            var B = A.Where(x => x.Count() >= 5).Select(x => x.ToUpper());
+            foreach (var item in B)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
