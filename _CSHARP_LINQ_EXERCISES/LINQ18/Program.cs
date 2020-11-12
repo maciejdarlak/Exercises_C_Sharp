5310acfbc3a33fee6bbfae372d4b37d453c9c660
maciejdarlak <maciej.darlak@gmail.com>
2020-11-12 15:30:25 +0100

    no message

--- _CSHARP_LINQ_EXERCISES/LINQ18/Program.cs
+++ _CSHARP_LINQ_EXERCISES/LINQ18/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace LINQ18
+namespace LINQ18 //Write a query that returns list of numbers and their squares only if square is greater than 20
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> A = new List<int>() { 3, 10, 20 };
+            var B = A.Where(x => x * x > 20);
+            foreach (var item in B)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
