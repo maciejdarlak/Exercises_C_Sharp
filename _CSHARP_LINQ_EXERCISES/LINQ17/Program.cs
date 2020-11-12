0e4016857d2e7465b62b59bf3147359a6c2d4f50
maciejdarlak <maciej.darlak@gmail.com>
2020-11-12 15:25:44 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ17 //Write a query that returns words starting with letter 'b' and ending with letter 'a'.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> A = new List<string>() { "Paris", "Barcelona", "Rome" };
+            var B = A.Where(x => x.StartsWith("B")).Where(x => x.EndsWith("a"));
+            foreach (var item in B)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
