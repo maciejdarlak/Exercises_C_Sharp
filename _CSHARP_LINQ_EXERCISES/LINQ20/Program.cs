78a961c23a9bc05ed20acbc9fba9cd4c5371b043
maciejdarlak <maciej.darlak@gmail.com>
2020-11-16 18:12:24 +0100

    no message

---
+++ _CSHARP_LINQ_EXERCISES/LINQ20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ20 //Write a query that returns only uppercase words from string.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "DIVINE, beat, Cat";
+            var bigLetterWords = A.Split(' ').Where(x => string.Equals(x, x.ToUpper()));
+            foreach (var item in bigLetterWords)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
