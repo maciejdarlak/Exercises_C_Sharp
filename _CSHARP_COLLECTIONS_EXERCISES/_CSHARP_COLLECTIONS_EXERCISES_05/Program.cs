b20e1baccce908e3dde310bc0f6002742d60f82b
DESKTOP-VCO1CNH\Maciek <maciej.darlak@gmail.com>
2021-06-24 10:24:39 +0200

    Added the  new 2 tasks.

---
+++ _CSHARP_COLLECTIONS_EXERCISES/_CSHARP_COLLECTIONS_EXERCISES_05/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+    Create a function that takes an array of numbers and returns the sum of the two lowest positive numbers.
+    Examples
+    SumSmallest([19, 5, 42, 2, 77]) ➞ 7
+    SumSmallest([10, 343445353, 3453445, 3453545353453]) ➞ 3453455
+    SumSmallest([2, 9, 6, -1]) ➞ 8
+    SumSmallest([879, 953, 694, -847, 342, 221, -91, -723, 791, -587]) ➞ 563
+    SumSmallest([3683, 2902, 3951, -475, 1617, -2385]) ➞ 4519
+*/
+
+
+using System;
+using System.Linq;
+
+
+namespace _CSHARP_COLLECTIONS_EXERCISES_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrueTimes trueTimes = new TrueTimes();
+            int[] array1 = new int[] { 19, 5, 42, 2, 77 };
+            var finalResult = trueTimes.SumSmallest(array1);
+            Console.WriteLine(finalResult);
+        }
+
+        public class TrueTimes
+        {
+            public int SumSmallest(int[] array1)
+            {
+                var array2 = array1.Select(x => x).OrderBy(x => x).ToArray();
+                var result = array2[0] + array2[1];
+                return result;
+            }
+        }
+    }
+}
---
+++ _CSHARP_COLLECTIONS_EXERCISES/_CSHARP_COLLECTIONS_EXERCISES_06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _CSHARP_COLLECTIONS_EXERCISES_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array1 = new object[] { 1, 2, "a", "b" };
+            TrueTimes trueTimes = new TrueTimes();
+            var result = trueTimes.ParseArray(array1);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public class TrueTimes
+        {
+            public string[] ParseArray(object[] array1)
+            {
+                var array2 = array1.Select(x => x.ToString()).ToArray();
+                return array2;
+
+                //var array2 = Array.ConvertAll(array1, x => x.ToString());
+                //return array2;
+            }
+        }
+    }
+}
